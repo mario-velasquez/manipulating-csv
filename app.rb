0bf59e8917eef159057210bade336af66ce3443e
Mario Velasquez <mario.velasquez.en@gmail.com>
2020-05-18 18:23:18 -0500

    Example CSV decoded

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'bundler'
 require 'smarter_csv'
+require 'json'
 #https://www.rubyguides.com/2018/10/parse-csv-ruby/
 puts "hi"
 
+data = SmarterCSV.process("examplefile.csv") 
 
-data = SmarterCSV.process("examplefile.csv")
+print(data.to_json)
