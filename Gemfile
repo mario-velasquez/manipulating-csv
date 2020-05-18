4828483bbcbc09a1c80d780d920c7ae0e398fa13
Mario Velasquez <mario.velasquez.en@gmail.com>
2020-05-18 16:45:46 -0500

    Fixed Gemfile for UNIX

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 
 source 'https://rubygems.org'
 gem 'smarter_csv'
+
--- app.rb
+++ app.rb
@@ -2,3 +2,6 @@ require 'bundler'
 require 'smarter_csv'
 #https://www.rubyguides.com/2018/10/parse-csv-ruby/
 puts "hi"
+
+
+data = SmarterCSV.process("examplefile.csv")
