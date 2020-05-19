8e8a545a483d7a741848e88eafcc92a69dbe3fdb
mario-velasquez-en <mario.velasquez.en@gmail.com>
2020-05-19 10:46:05 -0500

    Creating a CSV File

--- app.rb
+++ app.rb
@@ -7,3 +7,11 @@ puts "hi"
 data = SmarterCSV.process("examplefile.csv") 
 
 print(data.to_json)
+
+#Create's a CSV File if not created, wb will not append simply delete and redo.
+CSV.open('test.csv', 'wb') do |csv|
+    csv << ["Color", "Fruit"]
+    csv << ["Red",  "Apple"]
+    #csv << ["Yellow",  "Banana"]
+    csv << ["Orange", "Orange"]
+end
